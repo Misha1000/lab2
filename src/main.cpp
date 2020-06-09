1cdc923f78ce46401e4996eb4b28008a7508f3d7
misha1000 <misha.shepetko@gmail.com>
2020-06-10 00:46:52 +0300

    add check_usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+namespace {
+	void check_usage(int ac, char **av)
+	{
+		if (ac != 2)
+		{
+			std::cout << "Usage:\t" << av[0] << " dir_name" << std::endl;
+			std::cout << "\t\t\tdir_name - path to dir with input .csv files." << std::endl;
+			exit(1);
+		}
+	}
+}// namespace
+
 int		main(int ac, char **av)
 {
+	check_usage(ac, av);
 	return 0;
 }
